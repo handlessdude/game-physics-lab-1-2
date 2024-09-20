0bbf8c16a71f1c6fa3ca7f1d8b1c73396fff514e
Elizaveta Zalomnova <61969421+handlessdude@users.noreply.github.com>
2024-09-20 18:19:14 +0300

    Add explanations

--- Assets/Scripts/RobotManager.cs
+++ Assets/Scripts/RobotManager.cs
@@ -9,14 +9,15 @@ public class RobotManager : MonoBehaviour
     RobotJoint[] joints;
     public bool useForwardKinematics = true;
     
-    private void Start()
+    private void Awake()
     {
         joints = GetComponentsInChildren<RobotJoint>();
     }
     
     private void FixedUpdate()
     {
-        // angles[i] === local rotation for the i-th joint
+        // angles[i] === projection of the joint's local rotation vector onto its rotation axis
+        // each joint.Axis is already normalized
         float[] angles = joints.Select(joint => Vector3.Dot(joint.Axis, joint.transform.localRotation.eulerAngles)).ToArray();
 
         if (useForwardKinematics)
@@ -30,10 +31,11 @@ public class RobotManager : MonoBehaviour
     }
     
     // returns the position of the end effector, in global coordinates.
+    // For each joint, method calculates the next position by rotating the offset (distance from one joint to the next) using the cumulative rotation up to that joint
     private Vector3 ForwardKinematics(float[] angles)
     {
         Vector3 prevPoint = joints[0].transform.position;
-        Quaternion rotation = Quaternion.identity;
+        Quaternion rotation = Quaternion.identity; // indicates "no rotation"
         for (int i = 1; i < joints.Length; i++)
         {
             // Rotates around a new axis
